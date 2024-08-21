4e812a11e742e90ac4c59bcd54b77847a38e5990
cry9 <briple86@gmail.com>
2024-08-21 17:10:01 -0500

    Made numHits.py more versatile for different gaming needs.

--- numHits.py
+++ numHits.py
@@ -4,12 +4,13 @@ def num_hits():
 	b=randint(1,2)
 	if a==b:
 		c=input("Wanna try? (y/n): ")
+		d=randint(1,5)
 		print("")
 		a = 0
 		b = 0
 		if c != 'n' and c!= 'no'and c!='No' and c!='N':
 			print("")
-			print("Go once.")
+			print("Go",d,"time(s).")
 			print("")
 			c=0
 			num_hits()
@@ -20,7 +21,7 @@ def num_hits():
 		b=0
 		if c != 'n' and c!= 'no'and c!='No' and c!='N':
 			print("")
-			print("Try again.")
+			print("Try again. You may either go once or be skipped.")
 			print("")
 			c=0
 			num_hits()
